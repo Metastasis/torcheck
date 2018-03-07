eb2a09b89447c7e1fc4d591f131cbe7931924146
Metastasis <d1mq44@gmail.com>
2018-03-07 22:59:38 +0300

    added checks for transport layers

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 from dpkt.ip import IP
+from dpkt.tcp import TCP
+from dpkt.ssl import TLS, SSL2
 from netfilterqueue import NetfilterQueue
 
 LIBNETFILTER_QUEUE_NUM = 1
 
 
 def main_loop(packet):
-    pkt = IP(packet.get_payload())
+    packet.accept()
+    network = IP(packet.get_payload())
 
     # modify the packet all you want here
     # packet.set_payload(str(pkt)) #set the packet content to our modified version
 
-    print(pkt)
-    packet.accept()
+    transport = network.data
 
+    if type(transport) == TCP:
+        print("[!] tcp")
+
+    if type(transport) == TLS:
+        print("[!] tls")
+
+    if type(transport) == SSL2:
+        print("[!] sslv2")
+    print("[--------------------------]")
 
 nfqueue = NetfilterQueue()
 nfqueue.bind(LIBNETFILTER_QUEUE_NUM, main_loop)
