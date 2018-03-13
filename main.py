e9ef475498def7dbcd02e65c614db434e212189b
Metastasis <d1mq44@gmail.com>
2018-03-13 22:26:42 +0300

    checking Tor behaviour

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ from directory_ips import DA_IP_LIST
 
 LIBNETFILTER_QUEUE_NUM = 1
 
+counter = 0
+
 
 def egress_loop(packet):
+    global counter
     network = IP(packet.get_payload())
 
     # modify the packet all you want here
     # packet.set_payload(str(pkt)) #set the packet content to our modified version
+
+    counter += 1
 
     transport = network.data
 
@@ -20,12 +25,13 @@ def egress_loop(packet):
 
     if type(transport) == TCP:
         print("[!] tcp")
-    else:
-        packet.accept()
-        return
 
-    if transport.dport == 443 or transport.dport >= 9000 and transport.dport <= 9100:
-        print("[*] found relevant port: {}".format(transport.dport))
+    # if transport.dport == 443 or transport.dport >= 9000 and transport.dport <= 9100:
+    #     print("[*] found relevant port: {}".format(transport.dport))
+
+    if counter <= 10:
+        packet.drop()
+        return
 
     packet.accept()
 
---
+++ thoughts.md
@@ -0,0 +1,8 @@
+one of OR addresses 91.229.20.27:9001
+
+check this relay search (do they have API?)
+
+https://metrics.torproject.org/rs.html
+
+DA's list
+https://metrics.torproject.org/rs.html#search/flag:authority
