09aa76dcc3ec7d75d52f1fd7910b0a1b0705a6d5
Metastasis <d1mq44@gmail.com>
2018-03-13 22:45:47 +0300

    checking Tor behaviour

--- main.py
+++ main.py
@@ -6,30 +6,32 @@ from directory_ips import DA_IP_LIST
 
 LIBNETFILTER_QUEUE_NUM = 1
 
-counter = 0
+ip_list = []
 
 
 def egress_loop(packet):
-    global counter
+    global ip_list
     network = IP(packet.get_payload())
 
     # modify the packet all you want here
     # packet.set_payload(str(pkt)) #set the packet content to our modified version
-
-    counter += 1
 
     transport = network.data
 
     if network.dst in DA_IP_LIST:
         print("[!] FOUND DA [!]")
 
-    if type(transport) == TCP:
-        print("[!] tcp")
+    # if type(transport) == TCP:
+    #     print("[!] tcp")
 
-    # if transport.dport == 443 or transport.dport >= 9000 and transport.dport <= 9100:
-    #     print("[*] found relevant port: {}".format(transport.dport))
+    if transport.dport == 443 or transport.dport >= 9000 and transport.dport <= 9100:
+        print("[*] found relevant port: {}".format(transport.dport))
 
-    if counter <= 10:
+    if network.dst not in ip_list and len(ip_list) < 10:
+        print('Blacklisting: {}, length: {}'.format(network.dst, len(ip_list)))
+        ip_list.append(network.dst)
+
+    if network.dst in ip_list:
         packet.drop()
         return
 
@@ -47,4 +49,5 @@ try:
 except KeyboardInterrupt:
     print("Terminated")
 
+print(ip_list)
 nfqueue.unbind()
