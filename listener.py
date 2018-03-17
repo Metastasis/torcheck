bf986405f3d1547913838c84e3429aabaaef3b3c
Metastasis <d1mq44@gmail.com>
2018-03-17 21:23:13 +0300

    checking Tor behaviour

--- listener.py
+++ listener.py
@@ -1,9 +1,17 @@
-from stem.control import Controller
+from stem import StreamPurpose
+from stem.control import Controller, EventType
+
+
+def stream_cb(event):
+    if event.purpose != StreamPurpose.USER:
+        return
+
+    print('[circiut_id-stream_id] {}-{}'.format(event.curc_id, event.id))
+    print('requester is {}. ({}:{})'.format(event.source_addr, event.source_address, event.source_port))
+    print('destination is {}. ({}:{})'.format(event.target, event.target_address, event.target_port))
+
 
 with Controller.from_port(port=9051) as ctrl:
     ctrl.authenticate()
 
-    read = ctrl.get_info('traffic/read')
-    written = ctrl.get_info('traffic/written')
-
-    print('Read {} bytes. Received {} bytes.'.format(read, written))
+    ctrl.add_event_listener(stream_cb, EventType.STREAM)
