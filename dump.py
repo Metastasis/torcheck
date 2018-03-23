7c56e5c6a3369d8ffec9e23b7e911a045816765b
Metastasis <d1mq44@gmail.com>
2018-03-23 17:02:39 +0300

    checking Tor behaviour

--- dump.py
+++ dump.py
@@ -20,3 +20,14 @@ def dump(fp, stream):
             fp.write(line)
             cnt = 0
             line = ''
+
+    if line:
+        line = line + '\n'
+        fp.write(line)
+
+
+# if __name__ == '__main__':
+#     stream = b'\x31\x32\x34\x43\x39\x33\x67\x68\x69\x50\x78\x77\x66\x33\x29'
+#     fname = 'test.txt'
+#     with open('./tmp/' + fname, 'w') as fp:
+#         dump(fp, stream)
