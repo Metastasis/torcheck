8cdeb77c85d47d66f5a7112cba1d8739ba659ee2
Metastasis <d1mq44@gmail.com>
2018-03-23 18:14:34 +0300

    checking Tor behaviour

--- add_rules.sh
+++ add_rules.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-iptables -t filter -A OUTPUT -s 10.0.10.0/24 -j NFQUEUE --queue-num 1
-#iptables -t mangle -A INPUT -d 10.0.10.0/24 -j NFQUEUE --queue-num 1
-#iptables -A FORWARD -s 10.0.10.0/24 -j NFQUEUE --queue-num 1
+#iptables -A ONEW -o lo -j NFQUEUE --queue-num 1
+iptables -A OUTPUT -p tcp -o enp0s3 -j NFQUEUE --queue-num 1
+#iptables -A INPUT -p tcp -i enp0s3 -j NFQUEUE --queue-num 1
+#iptables -A FORWARD -p tcp -j NFQUEUE --queue-num 1
+
+#iptables -A OUTPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
+#iptables -A INPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
--- delete_rules.sh
+++ delete_rules.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-iptables -t filter -D OUTPUT -s 10.0.10.0/24 -j NFQUEUE --queue-num 1
-#iptables -t mangle -D INPUT -d 10.0.10.0/24 -j NFQUEUE --queue-num 1
-#iptables -D FORWARD -s 10.0.10.0/24 -j NFQUEUE --queue-num 1
+#iptables -D ONEW -o lo -j NFQUEUE --queue-num 1
+iptables -D OUTPUT -p tcp -o enp0s3 -j NFQUEUE --queue-num 1
+#iptables -D INPUT -p tcp -i enp0s3 -j NFQUEUE --queue-num 1
+#iptables -D FORWARD -p tcp -j NFQUEUE --queue-num 1
+
+#iptables -D OUTPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
+#iptables -D INPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
