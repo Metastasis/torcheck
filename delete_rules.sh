b47bf2296f85e636ce9ee6a072387dcb728bdc91
Metastasis <d1mq44@gmail.com>
2018-03-23 18:37:44 +0300

    checking Tor behaviour

--- delete_rules.sh
+++ delete_rules.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#iptables -D ONEW -o lo -j NFQUEUE --queue-num 1
-iptables -D OUTPUT -p tcp -o enp0s3 -j NFQUEUE --queue-num 1
-#iptables -D INPUT -p tcp -i enp0s3 -j NFQUEUE --queue-num 1
-#iptables -D FORWARD -p tcp -j NFQUEUE --queue-num 1
-
-#iptables -D OUTPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
-#iptables -D INPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
+#sudo iptables -D ONEW -o lo -j NFQUEUE --queue-num 1
+#sudo iptables -D OUTPUT -p tcp -o enp0s3 -j NFQUEUE --queue-num 1
+sudo iptables -D INPUT -p tcp -i enp0s3 -j NFQUEUE --queue-num 1
+#sudo iptables -D FORWARD -p tcp -j NFQUEUE --queue-num 1
+#sudo iptables -D OUTPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
+#sudo iptables -D INPUT -m owner --uid-owner tor -j NFQUEUE --queue-num 1
