5ff5a1a322b8f5f9c119bf047dfda917bb13633c
Metastasis <d1mq44@gmail.com>
2018-04-06 16:13:14 +0300

    checking Tor behaviour

--- listener.py
+++ listener.py
@@ -11,7 +11,15 @@ def stream_cb(event):
     print()
 
 
+def orconn_cb(event):
+    print('OR connection event:')
+    print('[{}-{}]'.format(event.endpoint_fingerprint, event.endpoint_nickname))
+    print('status is {}'.format(event.status))
+    print()
+
+
 def circ_cb(event):
+    print('Circuit event:')
     print('status: {}, id'.format(event.status, event.id))
     print('path: {}'.format(event.path))
     print('purpose: {}'.format(event.purpose))
@@ -23,12 +31,14 @@ if __name__ == '__main__':
     with Controller.from_port(port=9051) as ctrl:
         ctrl.authenticate()
 
-        ctrl.add_event_listener(stream_cb, EventType.STREAM)
+        ctrl.add_event_listener(orconn_cb, EventType.ORCONN)
+        ctrl.add_event_listener(circ_cb, EventType.CIRC)
 
         try:
             while True:
                 pass
         except KeyboardInterrupt:
             print('Exiting')
-            ctrl.remove_event_listener(stream_cb)
+            ctrl.remove_event_listener(orconn_cb)
+            ctrl.remove_event_listener(circ_cb)
             exit(1)
