9984ccd006e7d77e7902f310366b72934fe1a678
Metastasis <d1mq44@gmail.com>
2018-06-07 02:49:45 +0300

    make package

---
+++ torcheck/tests/configuration/test_base_config.py
@@ -0,0 +1,39 @@
+from unittest import TestCase, main
+from torcheck.configuration.base_config import BaseConfig
+
+
+class TestBaseConfig(TestCase):
+    def setUp(self):
+        self.LOAD_CONFIG_PATH = 'tests/configuration/config_for_load'
+        self.SAVE_CONFIG_PATH = 'tests/configuration/config_for_save'
+        self.result = [
+            '10.0.0.0',
+            '10.0.0.1',
+            '10.0.0.2'
+        ]
+
+    def tearDown(self):
+        try:
+            with open(self.SAVE_CONFIG_PATH, 'w'):
+                pass
+        except Exception as e:
+            print('Exception has been occurred while clearing file "{}":'.format(self.SAVE_CONFIG_PATH))
+
+    def test_load(self):
+        config = BaseConfig()
+        config.load(self.LOAD_CONFIG_PATH)
+        loaded = config.data
+        self.assertListEqual(self.result, loaded)
+
+    def test_save(self):
+        config = BaseConfig()
+        config.data = self.result
+        config.save(self.SAVE_CONFIG_PATH)
+        loaded_config = BaseConfig()
+        loaded_config.load(self.SAVE_CONFIG_PATH)
+        loaded = loaded_config.data
+        self.assertListEqual(self.result, loaded)
+
+
+if __name__ == '__main__':
+    main()
---
+++ torcheck/track/client_log.py
@@ -0,0 +1,58 @@
+from torcheck.settings import CLIENTLOG_PATH
+from datetime import datetime, timedelta
+import pickle
+import os
+
+
+class ClientLog:
+    def __init__(self):
+        self.ONE_MINUTE = timedelta(minutes=1)
+
+    def find_first_arrived(self, data):
+        copy = data.copy()
+        copy.reverse()
+        first_marked = next((k for k, v in enumerate(copy) if v[1] == 1), None)
+        if first_marked is None and len(data):
+            return len(data) - 1
+        if not len(data):
+            return None
+        search_from = len(data) - first_marked
+        rest_unmarked = data[search_from:]
+        first_arrived_idx = next((k for k, v in enumerate(rest_unmarked) if v[1] == 0), None)
+        if first_arrived_idx is None:
+            return None
+        return search_from + first_arrived_idx
+
+    def arrived_near(self, date):
+        data = []
+        if os.path.getsize(CLIENTLOG_PATH) <= 0:
+            return False
+        with open(CLIENTLOG_PATH, 'rb') as input:
+            data = pickle.load(input)
+        if not data or not len(data):
+            return False
+        time = date - self.ONE_MINUTE
+        last_arrived = [v for v in data if v[0] >= time]
+        if not len(last_arrived):
+            return False
+        arrived_idx = self.find_first_arrived(last_arrived)
+        if arrived_idx is None:
+            return False
+        [date, flag] = last_arrived[arrived_idx]
+        last_arrived[arrived_idx] = [date, 1]
+        with open(CLIENTLOG_PATH, 'wb') as output:
+            pickle.dump(last_arrived, output, pickle.HIGHEST_PROTOCOL)
+        return True
+
+    def log(self, date=datetime.now()):
+        data = []
+        if os.path.getsize(CLIENTLOG_PATH) > 0:
+            with open(CLIENTLOG_PATH, 'rb') as input:
+                data = pickle.load(input)
+                data.append([date, 0])
+        with open(CLIENTLOG_PATH, 'wb') as output:
+            pickle.dump(data, output, pickle.HIGHEST_PROTOCOL)
+
+    def clean(self):
+        with open(CLIENTLOG_PATH, 'wb'):
+            pass
